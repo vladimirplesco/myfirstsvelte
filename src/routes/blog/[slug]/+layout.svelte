b2eaf43322b64786d4bd905d5f277f4ecdb121b3
Vladimir Plesco <pleshco@deeplace.md>
2024-06-20 08:29:15 +0300

    Layout data

--- src/routes/blog/[slug]/+layout.svelte
+++ src/routes/blog/[slug]/+layout.svelte
@@ -3,9 +3,20 @@
 </script>
 
 <div class="layout">
-    <main>
-	<slot />
-    </main>
+  <main>
+		<slot />
+  </main>
+
+	<aside>
+		<h2>More posts</h2>
+		<ul>
+			{#each data.summaries as { slug, title }}
+				<li>
+					<a href="/blog/{slug}">{title}</a>
+				</li>
+			{/each}
+		</ul>
+	</aside>
 </div>
 
 <style>
